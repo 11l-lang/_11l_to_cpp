c39e6fcf75f1ef8f03df3443daca88f5a886d62d
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-10-16 20:20:14 +1000

    keyletters_to_keywords.py: added option --reverse

--- keyletters_to_keywords.py
+++ keyletters_to_keywords.py
@@ -1,11 +1,16 @@
 import sys, os, tokenizer
 
 if len(sys.argv) < 2:
-    print('Usage: python keyletters_to_keywords.py <in-file>')
+    print('Usage: python keyletters_to_keywords.py <in-file> [--reverse]')
     exit(1)
 
+reverse = '--reverse' in sys.argv
+expected_extention = '.11kw' if reverse else '.11l'
+if not sys.argv[1].endswith(expected_extention):
+    sys.exit('Input file should have extension `' + expected_extention + '`')
+
 source = open(sys.argv[1], encoding='utf-8-sig').read()
-outf   = open(os.path.splitext(sys.argv[1])[0] + '.11kw', 'w', encoding='utf8')
+outf   = open(os.path.splitext(sys.argv[1])[0] + ('.11l' if reverse else '.11kw'), 'w', encoding='utf8', newline = "\n")
 writepos = 0
 
 try:
@@ -16,7 +21,7 @@ try:
             if dot_pos == -1:                       # A dot_pos = token.value(source). {.find(‘.’) ? .len}
                 dot_pos = len(token.value(source))  # /
             outf.write(source[writepos:token.start])
-            outf.write(tokenizer.keywords[tokenizer.keywords.index(token.value(source)[:dot_pos]) % 11 + 11*2] + token.value(source)[dot_pos:])
+            outf.write(tokenizer.keywords[tokenizer.keywords.index(token.value(source)[:dot_pos]) % 11 + 11*2*(not reverse)] + token.value(source)[dot_pos:])
         else:
             outf.write(source[writepos:token.end])
         writepos = token.end
