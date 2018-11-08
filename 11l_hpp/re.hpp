0a200e4c833f1251ede853930b74aab6352e658e
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-11-08 19:22:34 +1000

    `re.split('pattern', 'string')` -> `?string?.split(re:?pattern?)` RegEx:Match:end,group

--- 11l_hpp/re.hpp
+++ 11l_hpp/re.hpp
@@ -4,6 +4,7 @@ namespace re
 {
 class RegEx
 {
+	friend class String;
 	std::wregex regex;
 public:
 	RegEx(const String &pattern) : regex((std::wstring&)pattern) {}
@@ -15,6 +16,8 @@ public:
 	public:
 		explicit operator bool() const {return !m.empty();}
 		int start(int group = 0) const {return (int)m.position(group);}
+		int end  (int group = 0) const {return int(m.position(group) + m.length(group));}
+		String group(int group_= 0) const {return String((std::u16string&&)m.str(group_));}
 	};
 
 	Match search(const String &s) const
@@ -29,4 +32,12 @@ inline RegEx _(const String &pattern)
 {
 	return RegEx(pattern);
 }
+}
+
+inline Array<String> String::split(const re::RegEx &regex) const
+{
+	Array<String> r;
+	for (std::wsregex_token_iterator it(((std::wstring*)this)->begin(), ((std::wstring*)this)->end(), regex.regex, -1), end; it != end; ++it)
+		r.append((std::u16string&&)it->str());
+	return r;
 }
