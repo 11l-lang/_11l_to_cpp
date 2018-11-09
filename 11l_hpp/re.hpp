7faf15311a5848095fa5cc8942fcb75a3f5b8385
Alexander Tretyak <alextretyak@users.noreply.github.com>
2018-11-09 20:56:43 +1000

    re.hpp: support GCC on Linux

--- 11l_hpp/re.hpp
+++ 11l_hpp/re.hpp
@@ -4,40 +4,67 @@ namespace re
 {
 class RegEx
 {
-	friend class String;
+	friend class ::String;
 	std::wregex regex;
 public:
+#ifdef _WIN32
 	RegEx(const String &pattern) : regex((std::wstring&)pattern) {}
+#else
+	RegEx(const String &pattern) : regex(std::wstring(pattern.cbegin(), pattern.cend())) {}
+#endif
 
 	class Match
 	{
 		friend class RegEx;
 		std::wsmatch m;
+#ifndef _WIN32
+		std::wstring ws;
+#endif
 	public:
 		explicit operator bool() const {return !m.empty();}
 		int start(int group = 0) const {return (int)m.position(group);}
 		int end  (int group = 0) const {return int(m.position(group) + m.length(group));}
+#ifdef _WIN32
 		String group(int group_= 0) const {return String((std::u16string&&)m.str(group_));}
+#else
+		String group(int group_= 0) const {std::wstring s = m.str(group_); return String(s.begin(), s.end());}
+#endif
 	};
 
 	Match search(const String &s) const
 	{
 		Match r;
-		std::regex_search((std::wstring&)s, r.m, regex);
+#ifdef _WIN32
+		std::regex_search((const std::wstring&)s, r.m, regex);
+#else
+		r.ws = std::wstring(s.cbegin(), s.cend());
+		std::regex_search(r.ws, r.m, regex);
+#endif
 		return r;
 	}
 
 	Match match(const String &s) const
 	{
 		Match r;
-		std::regex_match((std::wstring&)s, r.m, regex);
+#ifdef _WIN32
+		std::regex_match((const std::wstring&)s, r.m, regex);
+#else
+		r.ws = std::wstring(s.cbegin(), s.cend());
+		std::regex_match(r.ws, r.m, regex);
+#endif
 		return r;
 	}
 
 	Array<String> find_strings(const String &s) const
 	{
 		Array<String> r;
-		for (std::wsregex_iterator it(((std::wstring&)s).begin(), ((std::wstring&)s).end(), regex), end; it != end; ++it) {
+#ifdef _WIN32
+		for (std::wsregex_iterator it(((std::wstring&)s).begin(), ((std::wstring&)s).end(), regex), end; it != end; ++it)
+#else
+		std::wstring ws(s.cbegin(), s.cend());
+		for (std::wsregex_iterator it(ws.begin(), ws.end(), regex), end; it != end; ++it)
+#endif
+		{
 			int group = it->size() == 2 ? 1 : 0;
 			r.append(s[range_el((int)it->position(group), int(it->position(group) + it->length(group)))]);
 		}
@@ -46,7 +73,11 @@ public:
 
 	class Matches
 	{
+#ifdef _WIN32
 		String s;
+#else
+		std::wstring ws;
+#endif
 		std::wregex regex;
 		std::wsregex_iterator b;
 		class Iterator
@@ -61,7 +92,11 @@ public:
 			const decltype(*rit) &operator*() const {return *rit;}
 		} e;
 	public:
+#ifdef _WIN32
 		Matches(String &&s_, std::wregex &&regex_) : s(std::move(s_)), regex(std::move(regex_)), b(((std::wstring&)s).begin(), ((std::wstring&)s).end(), regex) {}
+#else
+		Matches(const String &s_, std::wregex &&regex_) : ws(s_.cbegin(), s_.cend()), regex(std::move(regex_)), b(ws.begin(), ws.end(), regex) {}
+#endif
 
 		Iterator begin() const {return Iterator(b);}
 		Iterator  &end()       {return e;}
@@ -92,7 +127,13 @@ inline Array<String> String::split(const re::RegEx &regex) const
 {
 	Array<String> r;
 	int begin = 0;
-	for (std::wsregex_iterator it(((std::wstring*)this)->begin(), ((std::wstring*)this)->end(), regex.regex), end; it != end; ++it) {
+#ifdef _WIN32
+	for (std::wsregex_iterator it(((std::wstring*)this)->begin(), ((std::wstring*)this)->end(), regex.regex), end; it != end; ++it)
+#else
+	std::wstring ws(cbegin(), cend());
+	for (std::wsregex_iterator it(ws.begin(), ws.end(), regex.regex), end; it != end; ++it)
+#endif
+	{
 		r.append(slice(begin, (int)it->position()));
 		begin = int(it->position() + it->length());
 	}
@@ -102,5 +143,11 @@ inline Array<String> String::split(const re::RegEx &regex) const
 
 String String::replace(const re::RegEx &regex, const String &repl) const
 {
+#ifdef _WIN32
 	return String((std::u16string&&)std::regex_replace((const std::wstring&)*this, regex.regex, (const std::wstring&)repl));
+#else
+	std::wstring ws(cbegin(), cend()), rws(repl.cbegin(), repl.cend());
+	std::wstring r = std::regex_replace(ws, regex.regex, rws);
+	return String(r.begin(), r.end());
+#endif
 }
