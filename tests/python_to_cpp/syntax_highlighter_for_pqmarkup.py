53f3ad196fefdb8727f96150e1fc27661d200739
Alexander Tretyak <alextretyak@users.noreply.github.com>
2019-01-19 11:23:13 +1000

    Start of work on transpilation of syntax_highlighter_for_pqmarkup

---
+++ tests/python_to_cpp/syntax_highlighter_for_pqmarkup.py
@@ -0,0 +1,23 @@
+import python_to_11l.tokenizer
+
+cat_to_class_python = {
+    python_to_11l.tokenizer.Token.Category.NAME                  : 'identifier',
+    python_to_11l.tokenizer.Token.Category.KEYWORD               : 'keyword',
+    python_to_11l.tokenizer.Token.Category.CONSTANT              : 'constant',
+    python_to_11l.tokenizer.Token.Category.OPERATOR_OR_DELIMITER : '',
+    python_to_11l.tokenizer.Token.Category.NUMERIC_LITERAL       : 'numeric-literal',
+    python_to_11l.tokenizer.Token.Category.STRING_LITERAL        : 'string-literal',
+    python_to_11l.tokenizer.Token.Category.INDENT                : '',
+    python_to_11l.tokenizer.Token.Category.DEDENT                : '',
+    python_to_11l.tokenizer.Token.Category.STATEMENT_SEPARATOR   : '',
+}
+
+class Error(Exception):
+    message : str
+    pos : int
+    def __init__(self, message, pos):
+        self.message = message
+        self.pos = pos
+
+def highlight(lang, source):
+    return source
