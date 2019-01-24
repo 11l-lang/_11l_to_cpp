8862efa9541738aa7261de5fa539b768eed2afa0
Alexander Tretyak <alextretyak@users.noreply.github.com>
2019-01-24 16:24:11 +1000

    Almost complete work on transpilation of syntax_highlighter_for_pqmarkup [for lang = 'Python']

--- tests/python_to_cpp/syntax_highlighter_for_pqmarkup.py
+++ tests/python_to_cpp/syntax_highlighter_for_pqmarkup.py
@@ -20,13 +20,32 @@ class Error(Exception):
         self.pos = pos
 
 def highlight(lang, source):
+    writepos = 0
     comments : List[Tuple[int, int]] = []
+    res = ''
+
+    def html_escape(s):
+        return s.replace('&', '&amp;').replace('<', '&lt;')
 
     if lang == 'Python':
-        for token in python_to_11l.tokenizer.tokenize(source, comments = comments):
-            print(token.to_str(source))
-        print(comments)
+        try:
+            for token in python_to_11l.tokenizer.tokenize(source, comments = comments) + [python_to_11l.tokenizer.Token(len(source), len(source), python_to_11l.tokenizer.Token.Category.STATEMENT_SEPARATOR)]:
+                while len(comments) and comments[0][0] < token.start:
+                    res += html_escape(source[writepos:comments[0][0]])
+                    writepos = comments[0][1]
+                    res += '<span class="comment">' + html_escape(source[comments[0][0]:comments[0][1]]) + '</span>'
+                    comments.pop(0)
+                res += html_escape(source[writepos:token.start])
+                writepos = token.end
+                css_class = cat_to_class_python[token.category]
+                if css_class != '':
+                    res += '<span class="' + css_class + '">' + html_escape(token.value(source)) + '</span>'
+                else:
+                    res += html_escape(token.value(source))
+
+        except python_to_11l.tokenizer.Error as e:
+            raise Error(e.message, e.pos)
     else:
         assert(lang == '11l')
 
-    return source
+    return res
