221ae02f396aced9d2c28b21fc73ac7f96f172c0
Alexander Tretyak <alextretyak@users.noreply.github.com>
2020-10-12 12:41:06 +1000

    Allow "\n", "\t" and ‘ ’ at the end of integer with base

--- 11l_hpp/String.hpp
+++ 11l_hpp/String.hpp
@@ -742,7 +742,7 @@ template <typename TInt> inline TInt to_int_t(const String &str)
 	if (*s == u'-') sign=-1, s++; else if (*s == u'+') s++;
 	for (; *s; s++) {
 		if (!Char(*s).is_digit()) {
-			while (*s == u' ' || *s == u'\t' || *s == u'\n')
+			while (*s == u' ' || *s == u'\t' || *s == u'\n') // why check for \n only at the end of the string with integer: \n at the beginning is very strange, while it's ok to have a file with integer ending with \n (so `int(open('...').read())` must work in this case)
 				s++;
 			if (*s == 0)
 				break;
@@ -773,22 +773,31 @@ template <typename TInt> inline TInt to_int_t(const String &str, int base)
 	while (*s && (*s == u' ' || *s == u'\t')) s++; // skip whitespace
 	if (*s == u'-') sign=-1, s++; else if (*s == u'+') s++;
 	for (; *s; s++) {
-		res *= base;
 		if (Char(*s).is_digit()) {
 			if (*s - u'0' >= base)
 				throw ValueError(str);
-			res += (*s - u'0');
+			res = res * base + (*s - u'0');
 		}
 		else if (base > 10) {
 			if (in(*s, range_el(u'A', char16_t(u'A' + base - 10))))
-				res += (*s - u'A' + 10);
+				res = res * base + (*s - u'A' + 10);
 			else if (in(*s, range_el(u'a', char16_t(u'a' + base - 10))))
-				res += (*s - u'a' + 10);
-			else
+				res = res * base + (*s - u'a' + 10);
+			else {
+				while (*s == u' ' || *s == u'\t' || *s == u'\n')
+					s++;
+				if (*s == 0)
+					break;
 				throw ValueError(str);
+			}
 		}
-		else
+		else {
+			while (*s == u' ' || *s == u'\t' || *s == u'\n')
+				s++;
+			if (*s == 0)
+				break;
 			throw ValueError(str);
+		}
 	}
 	return res * sign;
 }
